bf59da19359bbe8f79b28213f704b5fe043bc688
Francisco Galvis <pachosgalvis@gmail.com>
2024-05-19 17:05:09 -0500

    Config RetrofitClient into New Pet Class

--- app/src/main/java/com/componentes/vet_app/view/screens/NewPetScreen.kt
+++ app/src/main/java/com/componentes/vet_app/view/screens/NewPetScreen.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -13,11 +17,53 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.componentes.vet_app.R
+import com.componentes.vet_app.view.model.Pet
+import com.componentes.vet_app.view.model.connect.PetService
+import com.componentes.vet_app.view.model.connect.RetrofitClient
 import com.componentes.vet_app.view.navigation.Screen
 import com.componentes.vet_app.view.ui.theme.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Composable
 fun NewPetScreen(navController: NavController){
+
+    var petName by remember { mutableStateOf("") }
+    var petType by remember { mutableStateOf("") }
+    var petAge by remember { mutableStateOf("") }
+    var petBreed by remember { mutableStateOf("") }
+
+    // Función para guardar una nueva mascota
+    fun savePet() {
+        val newPet = Pet(
+            type = petType,
+            name = petName,
+            age = petAge.toIntOrNull() ?: 0,
+            breed = petBreed,
+            image = "placeholder.jpg",
+            id = 1
+        )
+
+        val petService = RetrofitClient.instance.create(PetService::class.java)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = petService.storePet(newPet)
+                if (response.isSuccessful) {
+                    // Mascota almacenado exitosamente
+                    navController.navigate(Screen.PetDetails.route)
+                } else {
+                    // Manejar el error
+                }
+            } catch (e: Exception) {
+                // Manejar la excepción
+            }
+        }
+    }
+
 
     Box(
         modifier = Modifier
